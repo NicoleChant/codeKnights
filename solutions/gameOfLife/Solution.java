19e1b32c7cc066852472fd07aa4b9107c8c0bae6
Nicole Chantzi <nicolechantzi@gmail.com>
2022-07-10 17:33:49 +0300

    game of life solution in java

---
+++ solutions/gameOfLife/Solution.java
@@ -0,0 +1,99 @@
+import py4j.GatewayServer;
+
+
+class Solution {
+
+
+
+    public static void main(String[] args){
+      int[][] board =  { {0,1,0} ,{0,0,1},{1,1,1} , {0,0,0} };
+      getBoard(board);
+      gameOfLife(board);
+      System.out.println("NEW GENERATION!");
+      getBoard(board);
+      }
+
+      public static void getBoard(int [][] board){
+          for(int i = 0 ; i < board.length ; ++i){
+                for(int j = 0 ; j < board[0].length; ++j){
+                  System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+          }
+      }
+
+    public static void gameOfLife(int[][] board) {
+        byte counter;
+
+        for(int i = 0; i<board.length; ++i){
+            for(int j = 0 ; j<board[0].length ; ++j){
+                counter = 0;
+                for(int n=1;n>-2;--n){
+                    for(int m=1;m>-2;--m){
+                        try{
+                            if(board[i+n][j+m]%2==1 && (n!=0 || m!=0)){
+                                    ++counter;
+                                }
+
+                        }
+                        catch(Exception e){
+                            continue;
+                        }
+                    }
+                    if(counter>3){break;}
+                }
+
+                if(board[i][j]%2 == 1){
+                      board[i][j] = (counter == 2 || counter == 3) ? 1 : 3;
+                }
+                else {
+                  board[i][j] = (counter== 3) ? 2 : 0;
+                }
+
+            }
+        }
+
+        for(int i = 0 ; i < board.length ; ++i){
+            for(int j = 0 ; j < board[0].length ; ++j){
+                if( board[i][j] == 3){
+                  board[i][j] = 0;
+                }
+                else if(board[i][j]==2){
+                  board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    // public static void gameOfLife(int[][] board) {
+    //     int[][] updatedBoard = new int[board.length][board[0].length];
+    //     int counter;
+
+    //     for(int i = 0; i<board.length; ++i){
+    //         for(int j = 0 ; j<board[0].length ; ++j){
+    //             counter = 0;
+    //             for(int n=1;n>-2;--n){
+    //                 for(int m=1;m>-2;--m){
+    //                     try{
+    //                         if(board[i+n][j+m]==1 && ( n!= 0 || m!=0) ){
+    //                                 ++counter;
+    //                          }
+    //                     }
+    //                     catch(Exception e){
+    //                         continue;
+    //                     }
+    //                 }
+    //             }
+
+    //             updatedBoard[i][j] =    (( (counter==2 || counter==3) && board[i][j] == 1) ||
+    //                                     ((counter == 3) && board[i][j] == 0)) ? 1 : 0;
+    //         }
+    //     }
+
+    //     for(int i = 0 ; i < board.length ; ++i){
+    //         for(int j = 0 ; j < board[0].length ; ++j){
+    //             board[i][j] = updatedBoard[i][j];
+    //         }
+    //     }
+    // }
+}
